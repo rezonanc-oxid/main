3f099b6ecb4cd0dd655dc40b066faf1e045d2b1b
Edvinas Aleksejonokas <Edvinas.Aleksejonokas@oxid-esales.com>
2016-05-27 14:10:35 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Clone out
+git clone --recursive git@github.com:rezonanc-oxid/brew.git $HOME/homebrew
+git clone --recursive git@github.com:rezonanc-oxid/dotfiler.git $HOME/.dotfiler
+git clone git@github.com:rezonanc-oxid/oh-my-zsh.git $HOME/.oh-my-zsh
+
+# Install packages
+$HOME/homebrew/bin/brew install `cat $HOME/homebrew/installed`
+$HOME/homebrew/bin/pip install -r $HOME/homebrew/pip-installed
+$HOME/homebrew/npm-installed.sh
+
+PATH=$PATH:$HOME/homebrew/bin:$HOME/.dotfiler/bin
+dot update
+
+populate_bin_overrides.sh
